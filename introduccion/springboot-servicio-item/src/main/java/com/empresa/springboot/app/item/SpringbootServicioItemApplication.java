47f8c15eb6c552bd8178854697619f3bf4c1e364
enrique-13 <enrikefen20@gmail.com>
2021-08-27 17:39:28 -0500

    Balanceo de carga con Ribbon / no-funciona

--- introduccion/springboot-servicio-item/src/main/java/com/empresa/springboot/app/item/SpringbootServicioItemApplication.java
+++ introduccion/springboot-servicio-item/src/main/java/com/empresa/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,8 +2,10 @@ package com.empresa.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@RibbonClient(name = "servicio-productos") //Colocamos el nommbre del servicio que se encuentra en el properties; si tuvieramos mas clientes configuramos @RibbonClients
 @EnableFeignClients //habilita clientes feing que tenemos implementados en el proyecto
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
--- introduccion/springboot-servicio-item/src/main/java/com/empresa/springboot/app/item/clientes/ProductoClienteRest.java
+++ introduccion/springboot-servicio-item/src/main/java/com/empresa/springboot/app/item/clientes/ProductoClienteRest.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.empresa.springboot.app.item.models.Producto;
 
-@FeignClient(name = "servicio-productos", url="localhost:8001") //Se declara que es un cliente feing
+@FeignClient(name = "servicio-productos") //Se declara que es un cliente feing
 public interface ProductoClienteRest {
 
 	@GetMapping("/listar")
